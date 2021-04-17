4200fd0b026604444073c09dabd808fae566ecc3
jonahtabb <jonahtabb@gmail.com>
2021-04-17 14:48:59 -0400

    Upload of Main Branch

--- index.js
+++ index.js
@@ -1,15 +1,167 @@
 const baseURL = "https://ghibliapi.herokuapp.com/films"
 
+//Declare and initialize the base url for retrieving the images
+const imgBaseUrl = "https://www.ghibli.jp/gallery"
+
+//Declare and initialize an array of objects that contains information about the images.
+//While the images are not available via the API, they are available via a free personal use license from Studio Ghibli. 
+//The id will be checked against the id in the Studio Ghibli API to assign the photo to its movie.
+//'imgSlug' == The slug that will be concatenated with the base url.  The 'title' property is just to make the data more human-readable.
+const imgEquiv = [
+    {imgSlug: "laputa", title: "Castle in the Sky", id: "2baf70d1-42bb-4437-b551-e5fed5a87abe"},
+    {imgSlug: "nausicaa", title: "Grave of the Fireflies", id: "12cfb892-aac0-4c5b-94af-521852e46d6a"},
+    {imgSlug: "totoro", title: "My Neighbor Totoro", id: "58611129-2dbc-4a81-a72f-77ddfc1b1b49"},
+    {imgSlug: "majo", title: "Kiki's Delivery Service", id: "ea660b10-85c4-4ae3-8a5f-41cea3648e3e"},
+    {imgSlug: "omoide", title: "Only Yesterday", id: "4e236f34-b981-41c3-8c65-f8c9000b94e7"},
+    {imgSlug: "porco", title: "Porco Rosso", id: "ebbb6b7c-945c-41ee-a792-de0e43191bd8"},
+    {imgSlug: "tanuki", title: "Pom Poko", id: "1b67aa9a-2e4a-45af-ac98-64d6ad15b16c"},
+    {imgSlug: "mimi", title: "Whisper of the Heart", id: "ff24da26-a969-4f0e-ba1e-a122ead6c6e3"},
+    {imgSlug: "mononoke", title: "Princess Mononoke", id: "0440483e-ca0e-4120-8c50-4c8cd9b965d6"},
+    {imgSlug: "yamada", title: "My Neighbors the Yamadas", id: "45204234-adfd-45cb-a505-a8e7a676b114"},
+    {imgSlug: "chihiro", title: "Spirited Away", id: "dc2e6bd1-8156-4886-adff-b39e6043af0c"},
+    {imgSlug: "baron", title: "The Cat Returns", id: "90b72513-afd4-4570-84de-a56c312fdf81"},
+    {imgSlug: "howl", title: "Howl's Moving Castle", id: "cd3d059c-09f4-4ff3-8d63-bc765a5184fa"},
+    {imgSlug: "ged", title: "Tales from Earthsea", id: "112c1e67-726f-40b1-ac17-6974127bb9b9"},
+    {imgSlug: "ponyo", title: "Ponyo", id: "758bf02e-3122-46e0-884e-67cf83df1786"},
+    {imgSlug: "karigurashi", title: "Arrietty", id: "2de9426b-914a-4a06-a3a0-5e6d9d3886f6"},
+    {imgSlug: "kokurikozaka", title: "From Up on Poppy Hill", id: "45db04e4-304a-4933-9823-33f389e8d74d"},
+    {imgSlug: "kazetachinu", title: "The Wind Rises", id: "67405111-37a5-438f-81cc-4666af60c800"},
+    {imgSlug: "kaguyahime", title: "The Tale of the Princess Kaguya", id: "578ae244-7750-4d9f-867b-f3cd3d6fecf4"},
+    {imgSlug: "marnie", title: "When Marnie Was There", id: "5fdfb320-2a02-49a7-94ff-5ca418cae602"},
+    {imgSlug: "redturtle", title: "The Red Turtle", id: "d868e6ec-c44a-405b-8fa6-f7f0f8cfb500"}
+
+]
+// console.log(imgEquiv);
+
+const cardContainer = document.getElementById('card-container')
+const cardRow = document.getElementById('card-row')
+const button = document.getElementById('random-image')
+console.log(button);
+
+//create random number string function
+function genRandomNum(){
+    let randomNum = Math.floor(Math.random() * 50) + 1 //generates a random number between 1 and 50
+    let randomNumString = randomNum.toString()
+    if (randomNumString.length == 1){
+        randomNumString = `00${randomNum}`
+    } else {
+        randomNumString = `0${randomNum}`
+    }
+    return randomNumString;
+}
+
+
+//generate a random image for each film
+button.addEventListener("click",() => {
+    console.log(filmsSummary);
+    console.log(cardList);
+    cardList.forEach(c => {
+        let imageLookup = imgEquiv.find(element => element.id == c.attributes[2].nodeValue);
+        let imageURL = `'${imgBaseUrl}/${imageLookup.imgSlug}${genRandomNum()}.jpg'`;
+        c.setAttribute("style", "background-image: url("+ imageURL +")") ;
+        
+        //console.log(c.style.backgroundImage);
+        
+    });
+});
+
+
+// //Apply fading to the cards //Not working
+//     cardContainer.classList.add("fade-start") //Set the card container to 0 opacity
+//     window.addEventListener("DOMContentLoaded", () => {
+//         window.setTimeout(function() {
+//             cardContainer.classList.replace("fade-start", "fade-end")
+//         }, 100)
+//     })
+
 function getFilmList() {
     fetch(baseURL)
         .then(res => res.json())
         .then(json => {
-            console.log(json);
-            let filmsSummary = json.map (film => {
-                return { title: film.title, description: film.description, id: film.id };
+             console.log(json);
+            filmsSummary = json.map (film => {
+                return { title: film.title, description: film.description, id: film.id, imageURL:"", release_date: film.release_date };
             })
-            console.log(filmsSummary);
-            }); 
+            return filmsSummary;
+        })
+
+        //Add image url to the 'filmsSummary' array
+        .then(filmsSummary => {
+            filmsSummary.forEach(film => {
+                let imageLookup = imgEquiv.find(element => element.id == film.id)
+                film.imageURL = `'${imgBaseUrl}/${imageLookup.imgSlug}${genRandomNum()}.jpg'`;
+                // console.log(imageLookup);
+            })
+            // console.log(filmsSummary);
+            return filmsSummary;
+        })
+
+        //Create the html element for each movie
+        .then(filmsSummary => {
+            //console.log(filmsSummary);
+            filmsSummary.forEach(film => {
+            //Create a Bootstrap column container for the new card, and insert it into the static parent div
+            let cardCol = document.createElement('div')
+            cardCol.className = "col-sm g-4"
+            cardRow.appendChild(cardCol)
+            
+            // let sampleImage = "https://www.ghibli.jp/gallery/ponyo001.jpg";
+            //Create a Bootstrap card
+            let card = document.createElement('div');
+            card.className = "card card-standard";
+
+            //Set Background Image For Card
+            card.setAttribute("style", "background-image: url("+ film.imageURL +")")
+            cardCol.appendChild(card);
+
+            //Set custom 'filmId' attribute for card
+            card.setAttribute("filmID", ""+film.id+"")
+            
+            //Set Title For Card
+            let titleContainer = document.createElement('div');
+            titleContainer.className = "container card-title";
+            card.appendChild(titleContainer);
+
+            //Create Title
+            let filmTitle = document.createElement('h4');
+            filmTitle.innerText = film.title;
+            titleContainer.appendChild(filmTitle);
+            })
+        })
+        
+        //Set a global reference cardList for assigning the overlays
+        .then( () => {
+            cardList = document.querySelectorAll('.card-standard');
+            // console.log(cardList);
+        })
+
+        .then( () => {
+            console.log(cardList);
+            //Create Overlay Cards
+            cardList.forEach(card => {
+                
+                //Create Overlay Card Container Element
+                let overlayCard = document.createElement('div');
+                overlayCard.className = "card card-overlay";
+                card.appendChild(overlayCard);
+
+                //Get Data from 'filmsSummary' array
+                let filmInfo = filmsSummary.find(element => element.id == card.attributes[2].nodeValue)
+                // console.log(filmInfo);
+
+                //Create a container for the text
+  
+                //Create Overlay Card Text Container Element
+                let overlayTextContainer = document.createElement('div');
+                overlayTextContainer.className = "col-md p-4 overlayTextContainer overflow-auto";
+                overlayTextContainer.innerText = filmInfo.description ;
+                overlayCard.appendChild(overlayTextContainer);
+            });
+            
+              //store a list of the cards
+        });
+
+
         }
 
 getFilmList(); 
